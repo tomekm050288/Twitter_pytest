cdd255b48ef135e5cdec491b852f0887a794ef5b
TomekM <tomasz_matuszewski@o2.pl>
2020-05-02 18:16:05 +0200

    fixtures, parametize

--- twitter.py
+++ twitter.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Twitter:
 
     version = '1.0'
@@ -6,7 +9,16 @@ class Twitter:
         self.tweets = []
 
     def tweet(self, message):
+        if len(message) > 160:
+            raise Exception("Message to long.")
         self.tweets.append(message)
+
+    def delete(self):
+        print("It's the end")
+
+    def find_hashtags(self, message):
+        return [m.lower() for m in re.findall("#(\w+) ?", message)]
+
 
 
 twitter = Twitter()
--- twitter_test.py
+++ twitter_test.py
@@ -1,21 +1,45 @@
-import unittest
+import pytest
+
 from twitter import Twitter
 
+#opcjonaly sposób wywołania funckji kończącej
+# @pytest.fixture
+# def twitter(request):
+#     twitter = Twitter()
+#     def fin():
+#         twitter.delete()
+#     request.addfinalizer(fin)
+#     return twitter
 
-class TwitterTest(unittest.TestCase):
-
-    def setUp(self):
-        self.twitter = Twitter()
-
-    def test_initialization(self):
-        self.assertTrue(self.twitter)
-
-    def test_tweet_single(self):
-        #when
-        self.twitter.tweet("Test message")
-        #then
-        self.assertEqual(self.twitter.tweets, ["Test message"])
+@pytest.fixture
+def twitter():
+    twitter = Twitter()
+    yield twitter
+    twitter.delete()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_twitter_initialization(twitter):
+    assert twitter
+
+
+def test_twitter_single_message(twitter):
+    twitter.tweet("Test message")
+    assert twitter.tweets == ["Test message"]
+
+
+def test_twitter_long_message(twitter):
+    with pytest.raises(Exception):
+        twitter.tweet("test"*41)
+
+
+@pytest.mark.parametrize("expected, message", (
+        (["first"], "Test #first message"),
+        (["first"], "#first message"),
+        (["first"], "#FIRST message"),
+        (["first"], "test message #first"),
+        (["first", "second"], "test message #first #second")
+        ))
+def test_tweet_with_hashtags(expected, message, twitter):
+    assert twitter.find_hashtags(message) == expected
+
+
