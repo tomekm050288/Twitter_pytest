e62b1e0f3333a5c138de4b1d10266e6166d6bafe
TomekM <tomasz_matuszewski@o2.pl>
2020-05-05 19:26:07 +0200

    monkey_patching

--- twitter_test.py
+++ twitter_test.py
@@ -2,11 +2,24 @@ import pytest
 
 from twitter import Twitter
 
+
 # nieporządana uruchamia się czy jest potrzebna czy nie przed kazdym testem
 # @pytest.fixture(autouse=True)
 # def prepare_backend_file():
 #     with open('test.txt', "w"):
 #         pass
+
+
+class ResponseGetMock(object):
+    def json(self):
+        return {'avatar_url': 'test'}
+
+
+# fixture to wyłączania zapytań z request
+@pytest.fixture(autouse=True)
+def no_request(monkeypatch):
+    monkeypatch.delattr('requests.sessions.Session.request')
+
 
 @pytest.fixture
 def backend(tmpdir):
@@ -15,14 +28,24 @@ def backend(tmpdir):
     return temp_file
 
 
+@pytest.fixture(params=[None, 'python'])
+def username(request):
+    return request.param
+
+
 # można wywołąć fixture dwa razy na jednej klasie
 # parametr name pozwala nadać inną nazwę funckji fixture niż zwracany parametr
 @pytest.fixture(params=['list', 'backend'], name='twitter')
-def fixture_twitter(backend, request):
+def fixture_twitter(backend, username, request, monkeypatch):
     if request.param == 'list':
-        twitter = Twitter()
+        twitter = Twitter(username=username)
     elif request.param == 'backend':
-        twitter = Twitter(backend=backend)
+        twitter = Twitter(backend=backend, username=username)
+
+    def monkey_return():
+        return 'test'
+
+    monkeypatch.setattr(twitter, 'get_user_avatar', monkey_return)
     return twitter
 
 
@@ -37,7 +60,7 @@ def test_twitter_single_message(twitter):
 
 def test_twitter_long_message(twitter):
     with pytest.raises(Exception):
-        twitter.tweet("test"*41)
+        twitter.tweet("test" * 41)
     assert twitter.tweet_messages == []
 
 
@@ -57,8 +80,14 @@ def test_initialize_two_twitter_classes(backend):
         (["first"], "#FIRST message"),
         (["first"], "test message #first"),
         (["first", "second"], "test message #first #second")
-        ))
+))
 def test_tweet_with_hashtags(expected, message, twitter):
     assert twitter.find_hashtags(message) == expected
 
 
+def test_tweet_with_username(twitter):
+    if not twitter.username:
+        pytest.skip()
+
+    twitter.tweet('Test message')
+    assert twitter.tweets == [{'message': 'Test message', 'avatar': 'test'}]
